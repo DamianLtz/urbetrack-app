1e09be513b553bec006b30958725d88ab113d16b
Damian Laterza <damianlaterza@gmail.com>
2026-07-04 12:50:43 -0300

    import del mapa de CABA en el proyecto

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,3 +1,17 @@
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer } from "react-leaflet";
+
+const CABA_CENTER: [number, number] = [-34.6037, -58.3816];
+
 export default function MapPage() {
-  return <h1 className="text-2xl font-bold">MapPage</h1>;
+  return (
+    <div className="h-[calc(100vh-4rem)] w-full">
+      <MapContainer center={CABA_CENTER} zoom={12} className="h-full w-full">
+        <TileLayer
+          attribution="&copy; OpenStreetMap contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  );
 }
