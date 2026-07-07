22eb240c719679712defca64ff9c459320208cd2
Damian Laterza <damianlaterza@gmail.com>
2026-07-07 13:16:09 -0300

    agregado rol del usuario en el componente navUser

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -26,6 +26,9 @@ export function NavUser({
 }) {
   const { isMobile } = useSidebar();
 
+  // Rol hardcodeado temporalmente:
+  const role = "Supervisor";
+
   return (
     <SidebarMenu>
       <SidebarMenuItem>
@@ -41,7 +44,7 @@ export function NavUser({
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-medium">{user.name}</span>
-                <span className="truncate text-xs">{user.email}</span>
+                <span className="truncate text-xs opacity-70">{role}</span>
               </div>
               <ChevronsUpDownIcon className="ml-auto size-4" />
             </SidebarMenuButton>
@@ -61,6 +64,9 @@ export function NavUser({
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-medium">{user.name}</span>
                   <span className="truncate text-xs">{user.email}</span>
+                  <span className="truncate text-xs text-violet-500">
+                    {role}
+                  </span>
                 </div>
               </div>
             </DropdownMenuLabel>
